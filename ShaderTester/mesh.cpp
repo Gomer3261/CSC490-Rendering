14e0d80e2eff13f5dbde4d6ba3ba0bfb4036c7d2
Geoffrey E. Gollmer <Geoff.Gollmer@Gmail.com>
2015-04-23 23:24:15 -0700

    Mesh memory optimization for GPU.

--- ShaderTester/mesh.cpp
+++ ShaderTester/mesh.cpp
@@ -3,14 +3,17 @@
 Mesh::Mesh(Material::Ptr material) :
     m_material(material),
     m_vertexBuffer(0),
+    m_indexBuffer(0),
     m_vertexData(),
     m_uvData(),
     m_normalData(),
     m_vertexIndicies(),
     m_uvIndicies(),
-    m_normalIndicies()
+    m_normalIndicies(),
+    m_vertexTable()
 {
     m_vertexCount = 0;
+    m_vertexDataCount = 0;
 }
 
 Mesh::~Mesh()
@@ -33,11 +36,17 @@ void Mesh::clearLoader() {
         delete m_normalData[i];
     }
     m_normalData.clear();
+    for(QMultiHash<uint, Mesh::MeshVertex*>::iterator iter = m_vertexTable.begin(); iter != m_vertexTable.end(); ++iter) {
+        delete iter.value();
+    }
+    m_vertexTable.clear();
 }
 
 void Mesh::clearVBO() {
     glDeleteBuffers(1, &m_vertexBuffer);
+    glDeleteBuffers(1, &m_indexBuffer);
     m_vertexBuffer = 0;
+    m_indexBuffer = 0;
 }
 
 void Mesh::addVertex(float x, float y, float z) {
@@ -81,53 +90,93 @@ void Mesh::setMaterial(Material::Ptr material)
 
 void Mesh::finalize() {
     qDebug() << "Finalizing Model:" << m_vertexCount << " vertices";
+
+    //Allocate for worst case scenario
+    //Cut the empty ends off when transfering to GPU.
     float *vertices = new float[3*m_vertexCount];
     float *uvs = new float[2*m_vertexCount];
     float *normals = new float[3*m_vertexCount];
 
-    int vertexIndex;
-    int uvIndex;
-    int normalIndex;
+    int *vertex_indices = new int[m_vertexCount];
+
+    Mesh::MeshVertex* test_vertex;
+    Mesh::MeshVertex* operating_vertex;
+    int vertex_array_index = 0;
 
     for(int i=0; i<m_vertexCount; i++) {
-        vertexIndex = m_vertexIndicies[i];
-        uvIndex = m_uvIndicies[i];
-        normalIndex = m_normalIndicies[i];
+        operating_vertex = NULL;
+        test_vertex = new Mesh::MeshVertex(m_vertexIndicies[i], m_uvIndicies[i], m_normalIndicies[i]);
 
-        vertices[(3*i)] = m_vertexData[vertexIndex]->x();
-        vertices[(3*i)+1] = m_vertexData[vertexIndex]->y();
-        vertices[(3*i)+2] = m_vertexData[vertexIndex]->z();
+        // Check if the vertex exists in the hashtable.
+        QMultiHash<uint, Mesh::MeshVertex*>::iterator iter = m_vertexTable.find(qHash(*test_vertex));
+        while(iter != m_vertexTable.end() && iter.key() == qHash(*test_vertex)) {
+            if((*iter.value()) == *test_vertex) {
+                operating_vertex = iter.value();
+                delete test_vertex;
+                break;
+            }
+            ++iter;
+        } // If no actual match is found, we continue to used the old index...
 
-        uvs[(2*i)] = m_uvData[uvIndex]->x();
-        uvs[(2*i)+1] = m_uvData[uvIndex]->y();
+        // Generating a new piece of vertex data for the GPU.
+        if(operating_vertex == NULL) {
+            operating_vertex = test_vertex;
 
-        normals[(3*i)] = m_normalData[normalIndex]->x();
-        normals[(3*i)+1] = m_normalData[normalIndex]->y();
-        normals[(3*i)+2] = m_normalData[normalIndex]->z();
+            vertices[(3*vertex_array_index)] = m_vertexData[operating_vertex->vertex_index()]->x();
+            vertices[(3*vertex_array_index)+1] = m_vertexData[operating_vertex->vertex_index()]->y();
+            vertices[(3*vertex_array_index)+2] = m_vertexData[operating_vertex->vertex_index()]->z();
+
+            uvs[(2*vertex_array_index)] = m_uvData[operating_vertex->uv_index()]->x();
+            uvs[(2*vertex_array_index)+1] = m_uvData[operating_vertex->uv_index()]->y();
+
+            normals[(3*vertex_array_index)] = m_normalData[operating_vertex->normal_index()]->x();
+            normals[(3*vertex_array_index)+1] = m_normalData[operating_vertex->normal_index()]->y();
+            normals[(3*vertex_array_index)+2] = m_normalData[operating_vertex->normal_index()]->z();
+
+            operating_vertex->setFinalIndex(vertex_array_index++);
+
+            //Ensure new data is correctly placed in the table.
+            m_vertexTable.insert(qHash(*operating_vertex), operating_vertex);
+        }
+
+        //Store final index for references vertex.
+        vertex_indices[i] = operating_vertex->final_index();
     }
+    //Store final vertex data count (<= vertex count)
+    m_vertexDataCount = vertex_array_index;
 
-    generateVBO(vertices, uvs, normals);
+    //Transfer data to GPU for storage.
+    generateVBO(vertices, uvs, normals, vertex_indices);
 
+    //Delete data off CPU.
     delete vertices;
     delete uvs;
     delete normals;
+    delete vertex_indices;
 }
 
-void Mesh::generateVBO(float* vertices, float* uvs, float* normals) {
+void Mesh::generateVBO(float* vertices, float* uvs, float* normals, int* vertex_indices) {
     if(m_vertexBuffer == 0) {
         qDebug() << "Creating VBO pointer";
         glGenBuffers(1, &m_vertexBuffer);
         glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+        glGenBuffers(1, &m_indexBuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
     } else {
         qDebug() << "Freeing VBO pointer";
         glDeleteBuffers(1, &m_vertexBuffer);
         glGenBuffers(1, &m_vertexBuffer);
         glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+        glDeleteBuffers(1, &m_indexBuffer);
+        glGenBuffers(1, &m_indexBuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
     }
-    glBufferData(GL_ARRAY_BUFFER, 8*m_vertexCount*sizeof(float), 0, GL_DYNAMIC_DRAW); //GL_STATIC_DRAW
-    glBufferSubData(GL_ARRAY_BUFFER, 0, 3*m_vertexCount*sizeof(float), vertices);
-    glBufferSubData(GL_ARRAY_BUFFER, 3*m_vertexCount*sizeof(float), 2*m_vertexCount*sizeof(float), uvs);
-    glBufferSubData(GL_ARRAY_BUFFER, 5*m_vertexCount*sizeof(float), 3*m_vertexCount*sizeof(float), normals);
+    glBufferData(GL_ARRAY_BUFFER, 8*m_vertexDataCount*sizeof(float), 0, GL_DYNAMIC_DRAW); //GL_STATIC_DRAW
+    glBufferSubData(GL_ARRAY_BUFFER, 0, 3*m_vertexDataCount*sizeof(float), vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, 3*m_vertexDataCount*sizeof(float), 2*m_vertexDataCount*sizeof(float), uvs);
+    glBufferSubData(GL_ARRAY_BUFFER, 5*m_vertexDataCount*sizeof(float), 3*m_vertexDataCount*sizeof(float), normals);
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_vertexCount*sizeof(int), vertex_indices, GL_STATIC_DRAW);
 }
 
 /* ===================================================
@@ -144,23 +193,31 @@ void Mesh::draw()
         for(int i=0; i<mat_shader->passCount(); i++) {
             mat_shader->beginGL(i);
 
+            // Bind model data buffers
             glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
 
+            // Set data availablility flags.
             glEnableClientState(GL_NORMAL_ARRAY);
             glEnableClientState(GL_TEXTURE_COORD_ARRAY);
             glEnableClientState(GL_VERTEX_ARRAY);
 
-            glNormalPointer(GL_FLOAT, 0, (void*)(5*m_vertexCount*sizeof(float)) );
-            glTexCoordPointer(2, GL_FLOAT, 0, (void*)(3*m_vertexCount*sizeof(float)) );
+            // Describe data format.
+            glNormalPointer(GL_FLOAT, 0, (void*)(5*m_vertexDataCount*sizeof(float)) );
+            glTexCoordPointer(2, GL_FLOAT, 0, (void*)(3*m_vertexDataCount*sizeof(float)) );
             glVertexPointer(3, GL_FLOAT, 0, 0);
 
-            glDrawArrays(GL_TRIANGLES, 0, m_vertexCount);
+            // Draw data to buffer.
+            glDrawElements(GL_TRIANGLES, m_vertexCount, GL_UNSIGNED_INT, 0);
 
+            // Reset data availability flags.
             glDisableClientState(GL_VERTEX_ARRAY);
             glDisableClientState(GL_TEXTURE_COORD_ARRAY);
             glDisableClientState(GL_NORMAL_ARRAY);
 
+            // Unbind buffers.
             glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
             mat_shader->endGL(i);
         }
--- ShaderTester/mesh.h
+++ ShaderTester/mesh.h
@@ -7,15 +7,23 @@
 #include <QVector3D>
 #include <QString>
 #include <QDebug>
+#include <QMultiHash>
 
 #include "material.h"
 #include "shadermanager.h"
+
+#define NBITS       5
+#define BMASK       0x037
+#define HASH(a, b, c)  (( (a&BMASK)<<NBITS|(b&BMASK) )<<NBITS|(c&BMASK))
+#define HSIZE       1<<NBITS*3
 
 class Mesh
 {
 public:
     Mesh(Material::Ptr material);
     ~Mesh();
+
+    class MeshVertex;
 
     void addVertex(float x, float y, float z);
     void addUV(float u, float v);
@@ -28,6 +36,7 @@ public:
 private:
     Material::Ptr m_material;
     GLuint m_vertexBuffer;
+    GLuint m_indexBuffer;
 
     QList<QVector3D*> m_vertexData;
     QList<QVector2D*> m_uvData;
@@ -36,11 +45,50 @@ private:
     QList<unsigned int> m_uvIndicies;
     QList<unsigned int> m_normalIndicies;
 
-    int m_vertexCount;
+    QMultiHash<uint, Mesh::MeshVertex*> m_vertexTable;
 
-    void generateVBO(float* vertices, float* uvs, float* normals);
+    int m_vertexCount;
+    int m_vertexDataCount;
+
+    void generateVBO(float* vertices, float* uvs, float* normals, int* vertex_indices);
     void clearLoader();
     void clearVBO();
+
+public:
+    class MeshVertex
+    {
+    public:
+        MeshVertex(int vert, int uv, int norm) :
+            m_vertex_index(vert),
+            m_uv_index(uv),
+            m_normal_index(norm),
+            m_final_index(-1)
+        {}
+
+        int vertex_index() const { return m_vertex_index; }
+        int uv_index() const { return m_uv_index; }
+        int normal_index() const { return m_normal_index; }
+        int final_index() { return m_final_index; }
+        void setFinalIndex(int index) { m_final_index = index; }
+
+    private:
+        int m_vertex_index;
+        int m_uv_index;
+        int m_normal_index;
+        int m_final_index;
+    };
 };
+
+inline bool operator==(const Mesh::MeshVertex &v1, const Mesh::MeshVertex &v2)
+{
+    return v1.vertex_index() == v2.vertex_index()
+           && v1.uv_index() == v2.uv_index()
+           && v1.normal_index() == v2.normal_index();
+}
+
+inline uint qHash(const Mesh::MeshVertex &key)
+{
+    return HASH(key.vertex_index(), key.uv_index(), key.normal_index());
+}
 
 #endif // MESH_H
